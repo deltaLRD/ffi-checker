18111d8366465312a87c10c5d28551aaba84a8de
liruidong <liruidong@ft.tech>
2024-12-30 13:28:22 +0800

    add c src

--- csrc/test1.c
+++ csrc/test1.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "test1.h"
 
 void greet() {
     printf("Hello, World!\n");
---
+++ csrc/test1.h
@@ -0,0 +1,5 @@
+#ifndef TEST1_H
+#define TEST1_H
+void greet();
+void* get_n_mem(int n);
+#endif
